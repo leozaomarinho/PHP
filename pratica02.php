be7af74a93e32e6cdc4579dc179a021dd91a6c3e
leonardo <leonardo@ABHNTBSEG6509.anima.intranet>
2022-01-28 15:46:53 -0300

    praticando switch basico

---
+++ pratica02.php
@@ -0,0 +1,18 @@
+<?php
+
+$opçao = '2';
+
+switch($opçao)
+{
+    case 1:
+        echo 'Você escolheu a primeira opção';
+        break;
+    case 2:
+        echo 'Você escolheu a segunda opção';
+        break;
+    default:
+        echo 'Opção invalida';
+        break;  
+}
+
+?>
