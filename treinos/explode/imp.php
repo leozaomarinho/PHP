0e9b29212ce60cbb10ad23a381fc478605bf5eea
Leonardo Marinho <81639502+leozaomarinho@users.noreply.github.com>
2023-03-29 21:11:01 -0300

    add implode e explode files

---
+++ treinos/explode/imp.php
@@ -0,0 +1,16 @@
+<?php
+
+$carros = ['uno','gol','fusion'];
+
+$str = implode(", ",$carros);
+
+echo "$str<br>";
+
+$frase = ["O","PHP","e","muito","legal"];
+
+$string = implode(" ",$frase);
+
+echo "$string<br>"
+
+
+?>
