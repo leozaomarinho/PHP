e933cd417b1c3072712346b4773687b59964f91d
Leonardo Marinho <81639502+leozaomarinho@users.noreply.github.com>
2023-04-10 22:19:17 -0300

    Add files via upload

---
+++ ex02.php
@@ -0,0 +1,46 @@
+<?php
+
+    class Humano {
+
+        public $olhos = 2;
+        public $bracos = 2;
+
+        public function falar(){
+
+            echo "Olá mundo!<br>";
+
+        }
+
+        private function CursoSuperior(){
+            $curso  = "Mestrado concluido";
+        }
+
+        public function getCurso($curso){
+           return this->curso;
+        }
+
+    };
+
+    class Professor extends Humano {
+        public $estudou = true;
+
+        public function Ensinar(){
+            echo "Repita comigo, vou aprender!<br>";
+        }
+    }
+
+    $leo = new Humano;
+
+    $luan = new Professor;
+
+    echo "$luan->bracos<br>";
+
+    $luan->falar();
+
+    
+
+    $luan->Ensinar();
+
+
+
+?>
